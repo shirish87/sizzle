7ea680396b0c9693e2bd17960a0a3de995b2514f
Shirish Kamath <shirishk.87@gmail.com>
2016-02-05 17:42:13 +0530

    Build: Reduce pollingTimeout

--- test/karma/karma.conf.js
+++ test/karma/karma.conf.js
@@ -13,7 +13,7 @@ module.exports = function( config ) {
 			timeout: 600, // 10 min
 			// BrowserStack has a limit of 120 requests per minute. The default
 			// "request per second" strategy doesn't scale to so many browsers.
-			pollingTimeout: 10000
+			pollingTimeout: 3000
 		},
 
 		// Can't specify path as "../../test" which would be intuitive
